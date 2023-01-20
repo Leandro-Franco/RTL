4c04d96289414e7b4dad751cbd123379e37f4332
Logistis <leandrofigueiredofranco@gmail.com.br>
2023-01-20 16:30:33 -0300

    priemiro commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+// import { render, screen } from '.@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('', () => {
+  test('', () => {
+    renderWithRouter(<App />);
+  });
+});
